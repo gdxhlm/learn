69d2e70f57fc833c91df1cc7d53d57720fa4c26c
smj <smj@ubuntu.(none)>
2020-10-10 14:05:54 +0800

    第一次提交

---
+++ test.c
@@ -0,0 +1,14 @@
+/*************************************************************************
+    > File Name: test.c
+    > Author: ma6174
+    > Mail: ma6174@163.com 
+    > Created Time: Sat 10 Oct 2020 01:57:48 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main(int argc, const char *argv[])
+{
+	printf("hello world\n");
+	return 0;
+}
